e0c6a82232cccbdbd021e16dac0b7077e7d26871
tchexp <tchexp@mail.ru>
2022-11-20 21:56:40 +0300

    example 5 if-else

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter your name: ");
+string userName = Console.ReadLine()+"-Сан";
+
+if (userName.ToLower() == "маша"+"-сан") 
+{
+    Console.WriteLine("Ура! Это же Маша!");
+}
+else
+{
+    Console.WriteLine("Привет " + userName);
+};
