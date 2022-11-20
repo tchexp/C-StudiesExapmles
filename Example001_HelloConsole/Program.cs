4d43639290d77a7f3f07455419e97106595c588e
tchexp <tchexp@mail.ru>
2022-11-20 20:20:44 +0300

    Example003 sum of two numbers added

--- Example001_HelloConsole/Program.cs
+++ Example001_HelloConsole/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello   ggg   World!")   \
+Console.WriteLine("Hello, World!");
---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Enter number A: "); 
+string a = Console.ReadLine();
+Console.WriteLine("Enter number B: "); 
+string b = Console.ReadLine();
+Console.WriteLine("Sum A and B is " + a+b);
